e79cf48bdefc8901c0316128f4c8b46344c0950e
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2023-04-30 12:45:26 +0700

    Update contact.php

--- frontend/contact.php
+++ frontend/contact.php
@@ -5,44 +5,8 @@
                 <h2>Contact <strong class='color'>Me</strong></h2>
             </div>
             <div class='row'>
-                <div class='col-md-8'>
-                    <div class='contact-form'>
-                        <form id='contact-form' data-toggle='validator' method='post' action='mail.php'>
-                            <div id='contact-form-result'></div>
-                            <div class='row'>
-                                <div class='col-md-6'>
-                                    <div class='form-group'>
-                                        <input type='text' class='form-control' placeholder='Name' name='name' required>
-                                        <div class='help-block with-errors'></div>
-                                    </div>
-                                </div>
-                                <div class='col-md-6'>
-                                    <div class='form-group'>
-                                        <input type='email' class='form-control' placeholder='Email' name='email'
-                                            required>
-                                        <div class='help-block with-errors'></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class='form-group'>
-                                <input type='text' class='form-control' placeholder='Subject' name='subject' required>
-                                <div class='help-block with-errors'></div>
-                            </div>
-                            <div class='form-group'>
-                                <textarea class='form-control' placeholder='Message' name='message' rows='5'
-                                    required></textarea>
-                                <div class='help-block with-errors'></div>
-                            </div>
-                            <div class='form-group text-center'>
-                                <button type='submit' class='btn-custom btn-color'>
-                                    Send Message
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class='col-md-4'>
-                    <div class='contact-info-icons'>
+                <div class='col-md-12'>
+                    <div class='contact-info-icons d-flex border justify-content-around gap-5 pt-3'>
                         <div class='contact-info'>
                             <i class="bi bi-house fs-3"></i>
                             <p><?= $user['residence'] ?></p>
